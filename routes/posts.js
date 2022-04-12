d51ea8ffebb53fb97a6b8bc5a6ae0f1b77ec511b
mohammed safvan <mohammedsafvannk@gmail.com>
2022-04-12 07:33:18 +0530

    completed CRUD operations

--- routes/posts.js
+++ routes/posts.js
@@ -4,11 +4,17 @@ const router = express.Router();
 const Post = require("../models/Post");
 
 // the url should like this "host:port/posts/"
-router.get("/", (req, res) => {
-  res.send("it is /posts GET");
+// This will return all the posts
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    res.json(err);
+  }
 });
 
-
+// Saving a post
 router.post("/", async (req, res) => {
   const post = new Post({
     title: req.body.title,
@@ -19,6 +25,45 @@ router.post("/", async (req, res) => {
     res.json(savedPost);
   } catch (err) {
     res.json({ msg: err });
+  }
+});
+
+// Getting a specific post
+router.get("/:postId", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId);
+    res.json(post);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+// Updating a post
+router.patch("/:postId", async (req, res) => {
+  try {
+    const updatePost = await Post.updateOne(
+      { _id: req.params.postId },
+      {
+        $set: {
+          title: req.body.title,
+          description: req.body.description,
+        },
+      }
+    );
+
+    res.send("Post updated");
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+// Deleting a post
+router.delete("/:postId", async (req, res) => {
+  try {
+    const deletedPost = await Post.deleteOne({ _id: req.params.postId });
+    res.json(deletedPost);
+  } catch (err) {
+    res.send(err);
   }
 });
 
